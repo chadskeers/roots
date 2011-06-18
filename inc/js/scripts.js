b14ad2c93343ebb19aa0dce21c3e554133c8379f
Chad Skeers <chadskeers@gmail.com>
2011-06-18 22:20:49 +0000

    Fix double switch bug for editors
    I hadn't taken care of the use case where someone tries to change
    to the html editor repeatedly, even though they're already looking
    at the html editor.

--- inc/js/scripts.js
+++ inc/js/scripts.js
@@ -5,12 +5,34 @@ jQuery(document).ready(function($){
 	if ($("#content").length) {
 		// If anyone can tell me a better way of getting access to the editor in other scripts, I'd love to hear it
 		// for now I'm just assigning it to the jQuery namespace
-		$.editor = CodeMirror.fromTextArea($("#content")[0], {mode: {name: "xml", htmlMode: true}});
+		var timer;
+		var visualButton = $("#edButtonPreview");
+		var htmlButton = $("#edButtonHTML");
+		visualButton.attr('onClick', '');
+		htmlButton.attr('onClick', '');
 		
-		// Save the code in CodeMirror to the real textarea every 5 seconds
-		// this is necesary for autosave to work... or we could override the autosave file,
-		// but this is easy and I don't think it should be a problem for webpage-size chunks of text
-		setInterval(function() { $.editor.save(); }, 5000);	
+		if (htmlButton.hasClass("active")) {
+			$.editor = CodeMirror.fromTextArea($("#content")[0], {mode: {name: "xml", htmlMode: true}});
+		} else {
+			$.editor = null;
+		}
 		
+		visualButton.click(function() {
+			if ($.editor !== null) {
+				$.editor.save();
+				$.editor.toTextArea();
+				$.editor = null;
+				switchEditors.go('content', 'tinymce')
+				clearInterval(timer);
+			}
+		});
+		
+		htmlButton.click(function() {
+			if ($.editor === null) {
+				switchEditors.go('content', 'html')
+				$.editor = CodeMirror.fromTextArea($("#content")[0], {mode: {name: "xml", htmlMode: true}});
+				timer = setInterval(function() { $.editor.save(); }, 5000);
+			}
+		});
 	}
 });
